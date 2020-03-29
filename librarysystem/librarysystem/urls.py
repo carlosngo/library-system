9f27e03e6f5b3bfeeab894c0aaa4da3f181266c1
Jeremy Dale Coronia <jeremy_coronia@dlsu.edu.ph>
2020-03-29 13:10:35 +0800

    Initial commit

--- librarysystem/librarysystem/urls.py
+++ librarysystem/librarysystem/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+from django.urls import include
+from django.urls import path
+
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
