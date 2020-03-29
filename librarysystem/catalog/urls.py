8292d2b88932b5184800b23b498a589aa8f6afa6
Jeremy Dale Coronia <jeremy_coronia@dlsu.edu.ph>
2020-03-29 18:29:30 +0800

    idk how to title this basta more update

--- librarysystem/catalog/urls.py
+++ librarysystem/catalog/urls.py
@@ -7,4 +7,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- librarysystem/catalog/views.py
+++ librarysystem/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from catalog.models import Book, Author, BookInstance, Publisher
@@ -24,4 +25,11 @@ def index(request):
 
     return render(request, 'index.html', context=context)
 
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    paginate_by = 10
 
