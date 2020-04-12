b4b836bd75fb5005a23d953846fb85ae302e13b7
Administrator <kathleen@zayls.com>
2020-04-12 17:09:56 +0800

    completely changed the design

--- librarysystem/catalog/static/js/toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-    var x = document.getElementById("hamburgerOverlay");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
