578855320704e9e64a100d570e40addcaaef3c41
Jeremy Dale Coronia <jeremy_coronia@dlsu.edu.ph>
2020-04-28 15:14:46 +0800

    changed Author Model

--- librarysystem/catalog/models.py
+++ librarysystem/catalog/models.py
@@ -76,6 +76,7 @@ class BookInstance(models.Model):
         return False
 
 class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
